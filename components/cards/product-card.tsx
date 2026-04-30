a30d405fad4060ce09039950340c54d4f49982b7
khusniridh0 <rmoh8394@gmail.com>
2026-04-30 15:03:07 +0700

    membuat sidebar kanan menjadi fix dan hanya bagian product yang scroll

--- components/cards/product-card.tsx
+++ components/cards/product-card.tsx
@@ -12,7 +12,7 @@ interface ProductCardProps {
 export const ProductCard = ({ product, onAdd }: ProductCardProps) => {
 
     return (
-        <div className="grid grid-cols-12 w-full gap-6 select-none cursor-pointer">
+        <div className="grid grid-cols-12 w-full gap-6 select-none cursor-pointer max-h-[calc(100vh-278px)] overflow-y-scroll scrollbar-hide">
             {product.map((item) => (
                 <div onClick={() => onAdd(item)} className="col-span-12 sm:col-span-6 lg:col-span-4 bg-[var(--color-base-dark-2)] rounded-lg text-center translate-y-[66px] h-[calc(100%-66px)] py-6 px-4" key={item.id} role="button" tabIndex={0}>
                     <div className="-translate-y-[66px]">
